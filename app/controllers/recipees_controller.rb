b221c41f402e0e45e5a8569c2f6f478b4b322711
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-03-22 12:39:59 +0630

    Add functionality for public recipe and added style

--- app/controllers/recipees_controller.rb
+++ app/controllers/recipees_controller.rb
@@ -1,5 +1,5 @@
 class RecipeesController < ApplicationController
-  before_action :set_recipee, only: %i[show edit update destroy]
+  before_action :set_recipee, only: %i[show update destroy]
 
   # GET /recipees or /recipees.json
   def index
@@ -7,15 +7,14 @@ class RecipeesController < ApplicationController
   end
 
   # GET /recipees/1 or /recipees/1.json
-  def show; end
+  def show
+  @recipe_foods = RecipeFood.where(recipee_id: params[:id])
+  end
 
   # GET /recipees/new
   def new
     @recipee = Recipee.new
   end
-
-  # GET /recipees/1/edit
-  def edit; end
 
   # POST /recipees or /recipees.json
   def create
@@ -36,7 +35,7 @@ class RecipeesController < ApplicationController
   # PATCH/PUT /recipees/1 or /recipees/1.json
   def update
     respond_to do |format|
-      if @recipee.update(recipee_params)
+      if @recipee.update(recipee_public_params)
         format.html { redirect_to recipee_url(@recipee), notice: 'Recipee was successfully updated.' }
         format.json { render :show, status: :ok, location: @recipee }
       else
@@ -67,4 +66,9 @@ class RecipeesController < ApplicationController
   def recipee_params
     params.require(:recipee).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
+
+  def recipee_public_params
+    params.require(:recipee).permit(:public)
+  end
+
 end
